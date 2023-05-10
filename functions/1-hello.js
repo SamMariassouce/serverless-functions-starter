361fbc7b46ec09aa9645bf78f599b8201c445398
Sam Mariassouce <sammariassouce@gmail.com>
2023-05-09 17:37:00 -1000

    1 example

--- functions/1-hello.js
+++ functions/1-hello.js
@@ -1,9 +1,18 @@
 // domain/.netlify/functions/1-hello
 // localhost:8888/
 
+// in node we use exports
+// event
+// context where the function is running , not used in this course
+
+// const person = { name: 'john' };
+
 exports.handler = async (event, context) => {
+  // console.log(event);
   return {
     statusCode: 200,
-    body: 'Supra supra supra cool',
+    // always a string or an object to stringify
+    body: 'Hello world Supra supra supra cool',
+    // body: JSON.stringify(person),
   };
 };
